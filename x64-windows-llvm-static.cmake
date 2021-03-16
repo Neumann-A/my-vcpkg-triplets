0cdb987f8851486c0828603d3395b7b0ced603e6
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-03-17 00:49:39 +0100

    more toolchain stuff to make it work.

--- x64-windows-llvm-static.cmake
+++ x64-windows-llvm-static.cmake
@@ -1,8 +1,8 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CRT_LINKAGE static)
-
-if(NOT PORT MATCHES "(boost|hwloc)")
+set(VCPKG_QT_TARGET_MKSPEC win32-clang-msvc)
+if(NOT PORT MATCHES "(boost|hwloc|libpq|icu|harfbuzz)")
     set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/x64-windows-llvm.toolchain.cmake")
     if(DEFINED VCPKG_PLATFORM_TOOLSET)
         set(VCPKG_PLATFORM_TOOLSET llvm)
@@ -13,5 +13,5 @@ set(VCPKG_POLICY_SKIP_ARCHITECTURE_CHECK enabled)
 set(VCPKG_POLICY_SKIP_DUMPBIN_CHECKS enabled)
 set(VCPKG_LOAD_VCVARS_ENV ON)
 
-set(VCPKG_C_FLAGS "/arch:AVX")
-set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS} /EHsc /GR")
+set(VCPKG_C_FLAGS "-arch:AVX")
+set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS} -EHsc -GR")
