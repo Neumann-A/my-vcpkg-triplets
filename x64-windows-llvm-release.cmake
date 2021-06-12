501e0285aaa7ac9dfd585b38dc8726c925704c55
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-06-12 09:20:23 +0200

    fix toolchains

--- x64-windows-llvm-release.cmake
+++ x64-windows-llvm-release.cmake
@@ -1,7 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CRT_LINKAGE static)
-set(VCPKG_QT_TARGET_MKSPEC win32-clang-msvc)
+set(VCPKG_CRT_LINKAGE dynamic)
 if(NOT PORT MATCHES "(boost|hwloc|libpq|icu|harfbuzz|qt*)")
     set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/x64-windows-llvm.toolchain.cmake")
     if(DEFINED VCPKG_PLATFORM_TOOLSET)
@@ -40,3 +39,5 @@ set(VCPKG_LOAD_VCVARS_ENV ON)
 
 set(VCPKG_C_FLAGS "-arch:AVX")
 set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS} -EHsc -GR")
+
+set(VCPKG_BUILD_TYPE release)
