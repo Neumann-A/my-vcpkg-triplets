aec82cc9a10818f58b2fc2774aee7fc9f91e1f0c
Neumann-A <30894796+Neumann-A@users.noreply.github.com>
2022-05-24 16:21:44 +0200

    reduce the duplication within the toolchains.

---
+++ x64-windows-llvm.toolchain.common.cmake
@@ -0,0 +1,61 @@
+include_guard(GLOBAL)
+
+include("${CMAKE_CURRENT_LIST_DIR}/${VCPKG_TARGET_TRIPLET}.cmake")
+
+# Set C standard.
+set(CMAKE_C_STANDARD 11 CACHE STRING "")
+set(CMAKE_C_STANDARD_REQUIRED ON CACHE STRING "")
+set(CMAKE_C_EXTENSIONS ON CACHE STRING "")
+set(std_c_flags "/std:c11 /Zc:__STDC__")
+
+# Set C++ standard.
+set(CMAKE_CXX_STANDARD 20 CACHE STRING "")
+set(CMAKE_CXX_STANDARD_REQUIRED ON CACHE STRING "")
+set(CMAKE_CXX_EXTENSIONS OFF CACHE STRING "")
+set(std_cxx_flags "/permissive- /std:c++20 /Zc:__cplusplus")
+
+# Set Windows definitions:
+set(windows_defs "/DWIN32 /D_WIN64 /D_WINDOWS /D_WIN32_WINNT=0x0A00 /DWINVER=0x0A00")
+string(APPEND windows_defs " /D_CRT_SECURE_NO_DEPRECATE /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_DEPRECATE")
+string(APPEND windows_defs " /D_ATL_SECURE_NO_DEPRECATE /D_SCL_SECURE_NO_WARNINGS")
+
+# Set runtime library.
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>$<$<STREQUAL:${VCPKG_CRT_LINKAGE},dynamic>:DLL>" CACHE STRING "")
+if(VCPKG_CRT_LINKAGE STREQUAL "dynamic")
+  set(VCPKG_CRT_FLAG "/MD")
+  set(VCPKG_DBG_FLAG "/Zi")
+elseif(VCPKG_CRT_LINKAGE STREQUAL "static")
+  set(VCPKG_CRT_FLAG "/MT")
+  set(VCPKG_DBG_FLAG "/Zi")
+else()
+  message(FATAL_ERROR "Invalid VCPKG_CRT_LINKAGE: \"${VCPKG_CRT_LINKAGE}\".")
+endif()
+
+# Set charset flag.
+set(CHARSET_FLAG "/utf-8")
+if(DEFINED VCPKG_SET_CHARSET_FLAG AND NOT VCPKG_SET_CHARSET_FLAG)
+  set(CHARSET_FLAG "")
+endif()
+
+set(CMAKE_CL_NOLOGO "/nologo" CACHE STRING "")
+
+# Set compiler flags.
+set(CLANG_FLAGS "/clang:-fasm /clang:-fopenmp-simd")
+# Disable logo for compiler and linker.
+if(NOT TOOLCHAIN_NO_LTO)
+  set(CLANG_C_LTO_FLAGS "/clang:-flto=full /clang:-fuse-ld=lld")
+  set(CLANG_CXX_LTO_FLAGS "/clang:-fwhole-program-vtables") # will be added in addition to CLANG_C_LTO_FLAGS
+else()
+  set(CLANG_C_LTO_FLAGS "")
+  set(CLANG_CXX_LTO_FLAGS "")
+endif()
+
+# Setup try_compile correctly. 
+list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES VCPKG_CRT_LINKAGE VCPKG_TARGET_ARCHITECTURE 
+                                                 VCPKG_C_FLAGS VCPKG_CXX_FLAGS
+                                                 VCPKG_C_FLAGS_DEBUG VCPKG_CXX_FLAGS_DEBUG
+                                                 VCPKG_C_FLAGS_RELEASE VCPKG_CXX_FLAGS_RELEASE
+                                                 VCPKG_LINKER_FLAGS VCPKG_LINKER_FLAGS_RELEASE VCPKG_LINKER_FLAGS_DEBUG
+                                                 VCPKG_SET_CHARSET_FLAG
+                                                 )
+
---
+++ x64-windows-llvm.toolchain.common.unset.cmake
@@ -0,0 +1,10 @@
+include_guard(GLOBAL)
+
+unset(CHARSET_FLAG)
+unset(CLANG_FLAGS)
+unset(CLANG_C_LTO_FLAGS)
+unset(CLANG_CXX_LTO_FLAGS)
+unset(windows_defs)
+unset(std_c_flags)
+unset(VCPKG_DBG_FLAG)
+unset(VCPKG_CRT_FLAG)
