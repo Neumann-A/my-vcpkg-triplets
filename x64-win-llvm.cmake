68dd776820fb7781f62a9d2b91d2d5ebae5577eb
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-08-13 17:34:35 +0200

    more documentation

--- x64-win-llvm.cmake
+++ x64-win-llvm.cmake
@@ -7,7 +7,7 @@ set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/x64-win-llvm/x64-win-llvm.toolchain.cmake")
 set(VCPKG_LOAD_VCVARS_ENV ON) # Setting VCPKG_CHAINLOAD_TOOLCHAIN_FILE deactivates automatic vcvars setup so reenable it!
 
-if(DEFINED VCPKG_PLATFORM_TOOLSET)
+if(DEFINED VCPKG_PLATFORM_TOOLSET) # Tricks vcpkg to load vcvars for a VCPKG_PLATFORM_TOOLSET which is not vc14[0-9]
     set(VCPKG_PLATFORM_TOOLSET ClangCL)
 endif()
 set(VCPKG_ENV_PASSTHROUGH_UNTRACKED "LLVMInstallDir;LLVMToolsVersion") # For the ClangCL toolset
