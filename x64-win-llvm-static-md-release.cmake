86fa5d83e1ed2588256a2feb0e2796e1fc986aa2
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-28 16:17:51 +0200

    fix mixup of library and crt linkage

--- x64-win-llvm-static-md-release.cmake
+++ x64-win-llvm-static-md-release.cmake
@@ -1,7 +1,7 @@
 # This triplet is tested in vcpkg ci via https://github.com/microsoft/vcpkg/pull/25897
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE static)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
 
 ## Toolchain setup
--- x64-win-llvm-static-md.cmake
+++ x64-win-llvm-static-md.cmake
@@ -1,7 +1,7 @@
 # This triplet is tested in vcpkg ci via https://github.com/microsoft/vcpkg/pull/25897
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE static)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
 ## Toolchain setup
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/x64-win-llvm/x64-win-llvm.toolchain.cmake")
